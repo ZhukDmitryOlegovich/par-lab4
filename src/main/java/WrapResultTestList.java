dfb89125cf946e8332afbb82d51b2d6152bf2c8e
ZhukDima <zhuk@gmail.com>
2021-12-04 00:20:48 +0300

    Scripted auto-commit on change (2021-12-04 00:20:48) by gitwatch.sh

--- src/main/java/WrapResultTestList.java
+++ src/main/java/WrapResultTestList.java
@@ -1,9 +1,12 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import java.util.List;
 
 public class WrapResultTestList {
     private final String packageId;
     private final List<ResultTest> resultTestList;
 
+    @JsonCreator
     public WrapResultTestList(String packageId, List<ResultTest> resultTestList) {
         this.packageId = packageId;
         this.resultTestList = resultTestList;
