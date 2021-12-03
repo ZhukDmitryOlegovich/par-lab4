8f3f134e4bd42a6f4e51081c0820cb6371e1434e
ZhukDima <zhuk@gmail.com>
2021-12-04 00:11:22 +0300

    Scripted auto-commit on change (2021-12-04 00:11:22) by gitwatch.sh

--- src/main/java/JSTestApp.java
+++ src/main/java/JSTestApp.java
@@ -2,7 +2,9 @@ import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
@@ -12,6 +14,7 @@ import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Future;
 
 public class JSTestApp extends AllDirectives {
@@ -22,8 +25,13 @@ public class JSTestApp extends AllDirectives {
         final Http http = Http.get(actorSystem);
         final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
         JSTestApp instance = new JSTestApp();
-//        final Flow<HttpRequest, HttpResponse, NotUsed> responseNotUsedFlow =
-//                instance.
+        final Flow<HttpRequest, HttpResponse, NotUsed> responseNotUsedFlow =
+                instance.createRoute(actorRef).flow(actorSystem, materializer);
+        final CompletionStage<ServerBinding> bindingCompletionStage = http.bindAndHandle(
+                responseNotUsedFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
     }
 
     private Route createRoute(ActorRef actorRouter) {
