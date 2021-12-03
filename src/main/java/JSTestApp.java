8fde9619deb4e3abdf0431b522a1adfc4adc1a8c
ZhukDima <zhuk@gmail.com>
2021-12-04 00:04:09 +0300

    Scripted auto-commit on change (2021-12-04 00:04:09) by gitwatch.sh

--- src/main/java/JSTestApp.java
+++ src/main/java/JSTestApp.java
@@ -1,9 +1,13 @@
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 public class JSTestApp extends AllDirectives {
     public static void main(String[] args) {
@@ -12,6 +16,8 @@ public class JSTestApp extends AllDirectives {
 
         final Http http = Http.get(actorSystem);
         final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
-        JSTestApp ins
+        JSTestApp instance = new JSTestApp();
+        final Flow<HttpRequest, HttpResponse, NotUsed> responseNotUsedFlow =
+                instance.
     }
 }
