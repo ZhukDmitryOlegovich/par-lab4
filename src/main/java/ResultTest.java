1b2b96d119f87bd3370994f30120da1b23b67bda
ZhukDima <zhuk@gmail.com>
2021-12-03 21:22:30 +0300

    Scripted auto-commit on change (2021-12-03 21:22:30) by gitwatch.sh

--- src/main/java/ResultTest.java
+++ src/main/java/ResultTest.java
@@ -1,3 +1,9 @@
+enum StatusTest {
+    PASSED,
+    FAILED,
+    CRASHED
+}
+
 public class ResultTest {
     public final InputTest inputTest;
     public final String receivedResult;
--- src/main/java/RunTest.java
+++ src/main/java/RunTest.java
@@ -5,11 +5,18 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
-public class ActorRunTest extends AbstractActor {
-    static public String execJS(String jscript, String functionName) throws ScriptException {
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+public class ActorTester extends AbstractActor {
+    private static final String SCRIPT_ENGINE_NAME = "nashorn";
+
+    static public String execJS(
+            String jscript, String functionName, Object[] params
+    ) throws ScriptException, NoSuchMethodException {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName(SCRIPT_ENGINE_NAME);
         engine.eval(jscript);
-        Invocable invocable = (Invocable) engine;
-        return invocable.invokeFunction(functionName, params).toString();
+        return ((Invocable) engine).invokeFunction(functionName, params).toString();
+    }
+
+    public runTest(InputTest inputTest) {
+
     }
 }
