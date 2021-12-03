702bdf01945949ff0dd6224a0a0583824ecb26f6
ZhukDima <zhuk@gmail.com>
2021-12-03 20:25:07 +0300

    Scripted auto-commit on change (2021-12-03 20:25:07) by gitwatch.sh

--- src/main/java/InputTest.java
+++ src/main/java/InputTest.java
@@ -1,6 +1,11 @@
 public class InputTest {
+    public final String testName;
+    public final Object[] params;
+    public final String expectedResult;
 
-    public InputTest(String testName, Object[] params, ) {
-
+    public InputTest(String testName, Object[] params, String expectedResult) {
+        this.testName = testName;
+        this.params = params;
+        this.expectedResult = expectedResult;
     }
 }
