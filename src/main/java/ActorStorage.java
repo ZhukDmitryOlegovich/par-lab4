7e0a94c840a617ae47cf2223bfad21d0eb4c53cf
ZhukDima <zhuk@gmail.com>
2021-12-03 20:41:06 +0300

    Scripted auto-commit on change (2021-12-03 20:41:06) by gitwatch.sh

--- src/main/java/ActorStorage.java
+++ src/main/java/ActorStorage.java
@@ -8,6 +8,6 @@ public class ActorStorage extends AbstractActor {
     private final Map<String, List<ResultTest>> storage = new HashMap<>();
 
     private void setResult(String id, ResultTest resultTest) {
-        storage.get()
+        storage.get(id)
     }
 }
