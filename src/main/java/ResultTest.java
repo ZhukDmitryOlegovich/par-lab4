422c1ee379d1edd83974873ea2470dd248457eb7
ZhukDima <zhuk@gmail.com>
2021-12-03 21:24:05 +0300

    Scripted auto-commit on change (2021-12-03 21:24:05) by gitwatch.sh

--- src/main/java/ResultTest.java
+++ src/main/java/ResultTest.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 enum StatusTest {
     PASSED,
     FAILED,
@@ -7,9 +9,15 @@ enum StatusTest {
 public class ResultTest {
     public final InputTest inputTest;
     public final String receivedResult;
+    public final StatusTest statusTest;
 
-    public ResultTest(InputTest inputTest, String receivedResult) {
+    public ResultTest(InputTest inputTest, String receivedResult, boolean isCrashed) {
         this.inputTest = inputTest;
         this.receivedResult = receivedResult;
+        this.statusTest = isCrashed
+                ? StatusTest.CRASHED
+                : Objects.equals(inputTest.expectedResult, receivedResult)
+                ? StatusTest.PASSED
+                : StatusTest.FAILED;
     }
 }
