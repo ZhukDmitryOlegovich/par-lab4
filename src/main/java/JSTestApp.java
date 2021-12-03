ba482759c325fcb4c2c1905d91a945e5e53514d8
ZhukDima <zhuk@gmail.com>
2021-12-04 00:02:28 +0300

    Scripted auto-commit on change (2021-12-04 00:02:28) by gitwatch.sh

--- src/main/java/JSTestApp.java
+++ src/main/java/JSTestApp.java
@@ -1,11 +1,17 @@
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
 import akka.http.javadsl.server.AllDirectives;
+import akka.stream.ActorMaterializer;
 
 public class JSTestApp extends AllDirectives {
     public static void main(String[] args) {
         ActorSystem actorSystem = ActorSystem.create("js_test_app");
         ActorRef actorRef = actorSystem.actorOf(Props.create(RouterTests.class));
+
+        final Http http = Http.get(actorSystem);
+        final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
+        JSTestApp ins
     }
 }
