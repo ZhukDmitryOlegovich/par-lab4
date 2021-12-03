4476fe0209cda1f7344955007ff3115edf89646e
ZhukDima <zhuk@gmail.com>
2021-12-03 23:37:15 +0300

    Scripted auto-commit on change (2021-12-03 23:37:15) by gitwatch.sh

--- src/main/java/WrapResultTestList.java
+++ src/main/java/WrapResultTestList.java
@@ -2,14 +2,10 @@ import java.util.List;
 
 public class WrapResultTestList {
     private final String packageId;
-    private final String jsScript;
-    private final String funcName;
     private final List<ResultTest> resultTestList;
 
-    public WrapResultTestList(String packageId, String jsScript, String funcName, List<ResultTest> resultTestList) {
+    public WrapResultTestList(String packageId, List<ResultTest> resultTestList) {
         this.packageId = packageId;
-        this.jsScript = jsScript;
-        this.funcName = funcName;
         this.resultTestList = resultTestList;
     }
 }
