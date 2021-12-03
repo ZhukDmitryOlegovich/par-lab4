94579ab66e83c9b19711773c7ac4605a422b1a81
ZhukDima <zhuk@gmail.com>
2021-12-04 00:41:08 +0300

    Scripted auto-commit on change (2021-12-04 00:41:08) by gitwatch.sh

--- src/main/java/InputTest.java
+++ src/main/java/InputTest.java
@@ -1,9 +1,13 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class InputTest {
     public final String testName;
     public final Object[] params;
     public final String expectedResult;
 
-    public InputTest(String testName, Object[] params, String expectedResult) {
+    public InputTest(
+            @JsonProperty("testName") String testName, Object[] params, String expectedResult
+    ) {
         this.testName = testName;
         this.params = params;
         this.expectedResult = expectedResult;
