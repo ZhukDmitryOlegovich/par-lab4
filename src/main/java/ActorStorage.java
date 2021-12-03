958dff86212ab26179fc38ea1db3c86c81526f02
ZhukDima <zhuk@gmail.com>
2021-12-03 20:30:54 +0300

    Scripted auto-commit on change (2021-12-03 20:30:54) by gitwatch.sh

--- src/main/java/ActorStorage.java
+++ src/main/java/ActorStorage.java
@@ -1,9 +1,14 @@
 import akka.actor.AbstractActor;
 
+import java.util.List;
 import java.util.Map;
 
 public class ActorStorage extends AbstractActor {
-    private final Map<String, ResultTest[]>
+    private final Map<String, List<ResultTest>> storage;
+
+    public ActorStorage(Map<String, List<ResultTest>> storage) {
+        this.storage = storage;
+    }
 
     private void setResult() {
 
