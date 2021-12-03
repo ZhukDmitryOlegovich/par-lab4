7dfd3ff0a741dd4159f2d2dac60bf5920ab49a41
ZhukDima <zhuk@gmail.com>
2021-12-03 21:05:01 +0300

    Scripted auto-commit on change (2021-12-03 21:05:01) by gitwatch.sh

--- src/main/java/RunTest.java
+++ src/main/java/RunTest.java
@@ -1,4 +1,15 @@
 import akka.actor.AbstractActor;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class ActorRunTest extends AbstractActor {
+    static public String execJS(String jscript, String functionName) throws ScriptException {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(jscript);
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(functionName, params).toString();
+    }
 }
