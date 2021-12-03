affcf96f3391e1ab97ddd6b617dac64c03388501
ZhukDima <zhuk@gmail.com>
2021-12-03 21:26:22 +0300

    Scripted auto-commit on change (2021-12-03 21:26:21) by gitwatch.sh

--- src/main/java/RunTest.java
+++ src/main/java/RunTest.java
@@ -8,7 +8,7 @@ import javax.script.ScriptException;
 public class ActorTester extends AbstractActor {
     private static final String SCRIPT_ENGINE_NAME = "nashorn";
 
-    static public String execJS(
+    static public Pair<Boolean, String> execJS(
             String jscript, String functionName, Object[] params
     ) throws ScriptException, NoSuchMethodException {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName(SCRIPT_ENGINE_NAME);
@@ -17,6 +17,6 @@ public class ActorTester extends AbstractActor {
     }
 
     public ResultTest runTest(InputTest inputTest) {
-        
+
     }
 }
